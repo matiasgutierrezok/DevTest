db31439943da6b28d37032e20505d76719cfd9da
Matias <mati.guti95@gmail.comm>
2022-05-26 00:36:58 -0300

    added search bar

--- src/screens/home-scr/home-scr.jsx
+++ src/screens/home-scr/home-scr.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Banner } from "../../shared/components/banner/banner";
 import { NavBar } from "../../shared/components/navbar/navbar";
+import { SearchBar } from "../../shared/components/search-bar/search-bar";
 
 export const HomeScr = () => {
     return(
         <div className="home-screen">
             <NavBar/>
             <Banner/>
+            <SearchBar/>
         </div>
     )
 }
---
+++ src/shared/components/search-bar/search-bar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SearchBar = () =>{
+    return(
+        <div className="search-bar">
+            <input type="search" placeholder="Search"/>
+            <span class="material-symbols-outlined">search</span>
+        </div>
+    )
+}
